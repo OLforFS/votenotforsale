7c00aea50dd818712efb000c9153a505b7c2c917
OLforFS <jonardbballeras@gmail.com>
2025-04-03 14:14:33 +0800

    making comment to work

---
+++ firebase-init.js
@@ -0,0 +1,17 @@
+// Firebase initialization script
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAWxGHZp6PhpNM71kB9w2yX3eFISvRYEf0",
+  authDomain: "votenotforsaleph.firebaseapp.com",
+  projectId: "votenotforsaleph",
+  storageBucket: "votenotforsaleph.firebasestorage.app",
+  messagingSenderId: "525832942491",
+  appId: "1:525832942491:web:2b94db6735e21efd92273e",
+  measurementId: "G-EBB1FJWWS9"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+console.log('Firebase initialized successfully');
